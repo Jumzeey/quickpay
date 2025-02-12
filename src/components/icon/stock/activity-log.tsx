e14fa7c4b6d36fa926147b9abdb3aa3e49a67ed8
Muhammad-awwal Bankole <awwalbankole@gmail.com>
2025-02-12 11:34:42 +0100

    fix: fixes to icons

--- src/components/icon/stock/activity-log.tsx
+++ src/components/icon/stock/activity-log.tsx
@@ -2,21 +2,49 @@ import React from "react";
 
 const SVG = () => {
   return (
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g clip-path="url(#clip0_2497_26427)">
-<path d="M3 4V18C3 19.1046 3.89543 20 5 20H17H19C20.1046 20 21 19.1046 21 18V8H17" stroke="#A4B0C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M3 4H17V18C17 19.1046 17.8954 20 19 20V20" stroke="#A4B0C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M13 8L7 8" stroke="#A4B0C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M13 12L9 12" stroke="#A4B0C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</g>
-<defs>
-<clipPath id="clip0_2497_26427">
-<rect width="24" height="24" fill="white"/>
-</clipPath>
-</defs>
-</svg>
-
-
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_2497_26427)">
+        <path
+          d="M3 4V18C3 19.1046 3.89543 20 5 20H17H19C20.1046 20 21 19.1046 21 18V8H17"
+          stroke="#A4B0C3"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M3 4H17V18C17 19.1046 17.8954 20 19 20V20"
+          stroke="#A4B0C3"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M13 8L7 8"
+          stroke="#A4B0C3"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M13 12L9 12"
+          stroke="#A4B0C3"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_2497_26427">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
   );
 };
 
